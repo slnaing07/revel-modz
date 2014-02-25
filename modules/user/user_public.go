5751d1765f6bed25b64b09b834f7d6a46156484c
tony <verdverm@gmail.com>
2014-02-25 15:40:54 -0500

    adding maillist module

--- modules/user/user_public.go
+++ modules/user/user_public.go
@@ -1,1 +1,26 @@
 package user
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/robfig/revel"
+)
+
+func GenerateNewUserId(db *gorm.DB) int64 {
+	var unique bool
+	var tries int
+	for !unique {
+		if tries >= 10 {
+			revel.ERROR.Println("tried", tries, "times to generate a unqiue id")
+			return 0
+		}
+		id := generateRandId()
+		u := GetUserBasicByUserId(db, id)
+		if u == nil {
+			return id
+		} else {
+			tries++
+		}
+	}
+	panic("shouldn't get here")
+	return -1
+}
