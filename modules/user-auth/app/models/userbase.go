45bda1c10d9ad7e53a907f19048a4db3f7772cc5
verdverm <verdverm@gmail.com>
2014-03-02 05:57:34 -0500

    removing old user-auth directory, updating mightymouse-js, adding websocket message communication module

--- modules/user-auth/app/models/userbase.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type UserBase struct {
-	UserId   int64  `json:"UserId"`
-	UserName string `json:"UserName"`
-	Email    string `json:"Email"`
-}
---
+++ modules/ws_comm/public/js/ws_comm.js
@@ -0,0 +1,75 @@
+var websock_comm = (function($) {
+
+    // private variables
+    var _socket;
+    var _handlers = {};
+    var _host = window.location.host;
+    var _url = "/comm"
+
+    // public interface
+    var WS_COMM = {};
+
+    WS_COMM.AddHandler = function(tag, handle) {
+        _handlers[tag] = handle;
+    }
+
+    WS_COMM.RemoveHandler = function(tag, handle) {
+        delete _handlers[tag];
+    }
+
+    WS_COMM.Connect = function(host, url) {
+        _host = host;
+        _url = url
+        _socket = new WebSocket('ws://' + host + '/comm');
+
+        _socket.onopen = function() {
+            console.log("Connection established");
+        };
+
+        // Log errors
+        _socket.onerror = function(error) {
+            console.log('WebSocket Error ' + error);
+            console.log(error);
+        };
+
+        _socket.onclose = function(e) {
+            console.log('Closing: ' + e.data);
+            console.log(e);
+        };
+
+        _socket.onmessage = function(e) {
+            console.log("Client <-- Server")
+            handleMessage(e.data);
+        }
+    }
+
+
+    WS_COMM.Send = function(msg) {
+        console.log("Client --> Server")
+        _socket.send(msg); // Send a nice message to the server
+    }
+
+    // private functions
+
+    function handleMessage(msg) {
+        // need to fix up returned message to remove '"' from start/end
+        var len = msg.length;
+        // msg = msg.substring(1, len - 1);
+        msg = msg.trim();
+        console.log("msg: " + msg.length);
+
+        var si = msg.indexOf(" ")
+        var tag = msg.substr(0, si)
+
+        if (tag in _handlers) {
+            console.log('message type: ' + tag);
+            handle = _handlers[tag]
+            var body = msg.substring(tag.length).trim();
+            handle(body)
+        } else {
+            console.log('Error: message type \"' + tag + '\" unknown');
+        }
+    }
+
+    return WS_COMM;
+}(jQuery));
